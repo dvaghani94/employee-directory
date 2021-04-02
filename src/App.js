72849e28e17949f317b66c7c843365d36284c882
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-02 15:13:41 -0400

    adding more file/folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import Directory from './pages/Directory.jsx'
 
 function App() {
   return (
-   
+   <Router basename='/user-directory'>
+     <div>
+       <Directory />
+     </div>
+   </Router>
   );
 }
 
---
+++ src/components/Table/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+import axios from 'axios';
+import  //import something using form
+
+class Table extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+
+        }
+    }
+
+    //handle input change to .sort through the names
+
+//componentDidMount lifestyle method will happen as the page loads
+componentDidMount(){
+    axios.get("https://randomuser.me/api/?results=25&nat=us")\
+    .then((res) => {
+        this.setState({ users: })
+    })
+}
+
+// render and return axios call info in here 
+render() {
+    return(
+        //return all our data and display to the page
+    )
+}
+}
+
