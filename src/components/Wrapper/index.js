bf306f9585dcbfed21751268fc42ae9757b15b7b
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-10 14:46:30 -0400

    changes to wrapper

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.css';
 
-function Wrapper({ children }) {
-    return <main className="wrapper">{ children }</main>
+function Wrapper(props) {
+    return <div className="wrapper">{props.children}</div>
 }
 
 export default Wrapper;
