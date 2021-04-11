284c2d888af931aab86ecb4ce2fea4911a74f9f9
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-10 23:44:36 -0400

    fixing header placement in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Container from './components/Container/index';
-import Header from './components/Header';
+import React from "react";
+import Container from "./components/Container/index";
+import Header from "./components/Header";
 import "./App.css";
-
 
 function App() {
   return (
-   <div className="App">
-       <Container />
-       <Header />
-     </div>
+    <div className="App">
+      <Header />
+      <Container />
+    </div>
   );
-};
+}
 
 export default App;
