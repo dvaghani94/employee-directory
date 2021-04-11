5f232ac3431f8f138afb645dcd153edd48fa3f9f
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-10 23:19:25 -0400

    reverting come changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./style";
+import React from 'react';
+import './style.css'
 
-const Header = () => {
-  return (
-    <div>
-      <header>
-        <h1>Employee Directory</h1>
-        <p>Input name in search field to search a specific employee</p>
-      </header>
-    </div>
-  );
+function Header(props) {
+    return (
+        <div>
+            <header>
+                <h1>Employee Directory</h1>
+                <p>Input name in search field to search a specific employee</p>
+            </header>
+        </div>
+    );
 };
 
 export default Header;
